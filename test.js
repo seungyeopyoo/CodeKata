27e3d4b8514d9adcbb7e19604a25b5c46c8f9625
yooseungyeop <yooseungyub@naver.com>
2024-06-24 11:25:22 +0900

    day42삼총사

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
-let iterable = 'try hello world';
-
-for (let value of iterable) {
-	console.log(value);
+let number = [-1, 1, -1, 1];
+let count = 0;
+for (let i = 0; i < number.length - 2; i++) {
+	for (let j = i + 1; j < number.length - 1; j++) {
+		for (let k = j + 1; k < number.length; k++) {
+			console.log(`i: ${i}, j: ${j}, k: ${k}`);
+			count++;
+		}
+	}
 }
-// let array = Array.from(iterable);
-// array.forEach((element) => {
-// 	console.log(element);
-// });
+console.log(`${count}`);
