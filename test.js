e1e9652ca17e28bb5c21875ff1c2b478af3f31e9
yooseungyeop <yooseungyub@naver.com>
2024-06-25 17:13:39 +0900

    day43크기가 작은 부분문자열

--- test.js
+++ test.js
@@ -1,11 +1,5 @@
-let number = [-1, 1, -1, 1];
-let count = 0;
-for (let i = 0; i < number.length - 2; i++) {
-	for (let j = i + 1; j < number.length - 1; j++) {
-		for (let k = j + 1; k < number.length; k++) {
-			console.log(`i: ${i}, j: ${j}, k: ${k}`);
-			count++;
-		}
-	}
-}
-console.log(`${count}`);
+let t = '123231';
+const sliceT = t.slice();
+console.log(sliceT);
+
+// 123 232 323 231
