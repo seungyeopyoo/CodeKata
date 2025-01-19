71d388be94699dcef325c4819732120499e57aae
yooseungyeop <yooseungyub@naver.com>
2025-01-20 02:32:39 +0900

    ibmunDay13-2

--- test.js
+++ test.js
@@ -1,13 +1,14 @@
-// let q = [];
+let q = '.... . .-.. .-.. ---';
 // let x = (23 + '').split('');
-// // let t = x.split('');
+// let t = x.split('');
+let x = q.split(' ');
 
-// console.log(x);
+console.log(x);
 
 // // let z = t.join('');
 
 // // console.log(z);
 
-let q = parseInt(23 / 5);
+// let q = parseInt(23 / 5);
 
-console.log(q);
+// console.log(q);
