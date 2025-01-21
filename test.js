fd815a62eb6033ec29ea9275c781a08758a4ed98
yooseungyeop <yooseungyub@naver.com>
2025-01-21 23:36:35 +0900

    ibmunDay14

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-let q = '.... . .-.. .-.. ---';
+// let q = '.... . .-.. .-.. ---';
 // let x = (23 + '').split('');
 // let t = x.split('');
-let x = q.split(' ');
+// let x = q.split(' ');
 
-console.log(x);
+// console.log(x);
 
 // // let z = t.join('');
 
@@ -12,3 +12,10 @@ console.log(x);
 // let q = parseInt(23 / 5);
 
 // console.log(q);
+
+let a = [1, 2, 3, 4, 6, 5];
+a.slice(2);
+let q = [];
+let z = q.push(a);
+console.log(q);
+console.log(a);
