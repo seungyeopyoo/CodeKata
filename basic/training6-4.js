67e529d6ebe1c0420ed9d5eae07f45410b54ca49
yooseungyeop <yooseungyub@naver.com>
2025-03-05 23:00:05 +0900

    training6-4

---
+++ basic/training6-4.js
@@ -0,0 +1,24 @@
+// 공배수
+// 정수 number와 n, m이 주어집니다.
+// number가
+// n의 배수이면서 m의 배수이면 1을 아니라면 0을 return하도록 solution 함수를 완성해주세요.
+
+// 제한사항
+// 10 ≤ number ≤ 100
+// 2 ≤ n, m < 10
+// 입출력 예
+// number	n	m	result
+// 60	,2	,3	1
+// 55,	10	,5	0
+
+// 60은 2의 배수이면서 3의 배수이기 때문에 1을 return합니다.
+// 55는 5의 배수이지만 10의 배수가 아니기 때문에 0을 return합니다.
+
+// step 1. number % n === 0 && num % m === 0 return 1 :0
+
+const solution = function (number, n, m) {
+	return number % n === 0 && number % m === 0 ? 1 : 0;
+};
+
+console.log(solution(60, 2, 3));
+console.log(solution(55, 10, 5));
